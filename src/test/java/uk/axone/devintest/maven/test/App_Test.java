174170e78ac66b9da8157e578bbf901fc13a79be
axoneuk <info@axone-tech.uk>
2020-02-09 06:55:04 +0000

    commiting the project

---
+++ src/test/java/uk/axone/devintest/maven/test/App_Test.java
@@ -0,0 +1,18 @@
+package uk.axone.devintest.maven.test;
+
+import org.junit.Test;
+
+public class App_Test {
+
+    @Test
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+
+
+    @Test
+    public void test2(){
+        System.out.println("Running test 2");
+    }
+
+}
